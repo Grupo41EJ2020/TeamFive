703c959a0bd6a14920b98719c1c70a6ae6e0a014
Jonhatan Azael Ortiz Gonzalez <jonatan.ortiz3817@gmail.com>
2020-05-28 14:00:38 -0500

    Modelo,Interface,Repositorio de Curso Tema

---
+++ Models/iCurso_Tema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCLaboratorio.Models
+{
+    public interface iCurso_Tema
+    {
+        List<Curso_Tema> obtenerCurso_Temas();
+        Curso_Tema obtenerCurso_Tema(int idCurso_Tema);
+        void insertarCurso_Tema(Curso_Tema datosCurso_Tema);
+        void eliminarCurso_Tema(int idCurso_Tema);
+        void actualizarCurso_Tema(Curso_Tema datosCurso_Tema);
+    }
+}
